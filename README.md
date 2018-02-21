3c05260af9d09fb1d21e871a586187d83f132539
Alexander Ritter <braydelritter@gmail.com>
2018-02-21 09:24:02 -0600

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SMTPEmailSender
 A multi threaded email sender, that sends a email to a email address.
+
+This project is mostly based off of the work of Liu Junfeng (https://www.codeproject.com/Articles/26663/Email-Sender)
+
+Ive took all of his work, and used it to create a multithreaded portable library
+that will be used in future project's to verify the user when reseting the password.
+
+To use, just use the mail loader at the namespace 
+indiegoat.SMTP.loader(string From, string To, string Subject, string Body)
---
+++ SMTPEmailSender.cs/SMTPEmailSender/Main.cs
@@ -0,0 +1,25 @@
+﻿using QiHe.CodeLib.Net;
+using System;
+using System.Threading;
+
+namespace IndieGoat.SMTP
+{
+    public static class SMTPEmailSender
+    {
+        public static void SendEmail(string from, string to, string subject, string body)
+        {
+            bool Result = EmailSender.Send(from, to, subject, body);
+            if (Result) Console.WriteLine("Sent mail!");
+            if (!Result) Console.WriteLine("Sent mail failed!");
+        }
+        public static void SendEmailAsync(string from, string to, string subject, string body)
+        {
+            new Thread(new ThreadStart(() =>
+            {
+                bool Result = EmailSender.Send(from, to, subject, body);
+                if (Result) Console.WriteLine("Sent mail!");
+                if (!Result) Console.WriteLine("Sent mail failed!");
+            })).Start();
+        }
+    }
+}
